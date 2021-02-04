d44e8af29e719f454b17c596f92b40e003220f2d
ShreyasBoss <shdeshpande1994@gmail.com>
2021-02-04 18:05:49 +0530

    second try

--- asset/app.js
+++ asset/app.js
@@ -2,8 +2,8 @@
  var bgimg = document.getElementById('img');
  var img2 = document.getElementById('img2');
 
-let beep = new Audio('/asset/beep.mp3')
-let msg = new Audio('/asset/b2.mp3')
+let beep = new Audio('https://shreyasboss.github.io/electionDemo2/asset/beep.mp3')
+let msg = new Audio('https://shreyasboss.github.io/electionDemo2/asset/b2.mp3')
 function buttonclicked(){
 beep.play()
 img2.style.display='block';
